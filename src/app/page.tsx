c644dd29386e957188991e3430c42cc10ec0188a
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-08-08 23:53:18 +0200

    Change message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,12 @@ export default function Home() {
     <>
       <P5Sketch/>
       <main className="min-h-screen flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-white text-center">
-          Pablo Apausa
+        <h1 className="text-4xl text-white text-center">
+          Work in progress
         </h1>
+        <h2 className="text-2xl text-white text-center">
+          Come back later!
+        </h2>
       </main>
     </>
   );
