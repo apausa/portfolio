25d890dfa1be79a30e6470006ca492acf962704a
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-08-09 13:10:26 +0200

    Develop orb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
     <>
       <P5Sketch/>
       <main className="min-h-screen flex flex-col items-center justify-center">
-        <h1 className="text-4xl text-white text-center">
-          Work in progress
-        </h1>
-        <h2 className="text-2xl text-white text-center">
-          Come back later!
-        </h2>
-      </main>
+      <h1 className="text-4xl text-white text-center">
+        Work in progress
+      </h1>
+      <h2 className="text-2xl text-white text-center">
+        Come back later!
+      </h2>
+    </main>
     </>
   );
 }
