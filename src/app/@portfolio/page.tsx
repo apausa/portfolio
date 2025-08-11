95b4315e6cfe87bac6d8967672fc38e3f721fb10
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-08-11 15:04:22 +0200

    Design interface

--- src/app/@portfolio/page.tsx
+++ src/app/@portfolio/page.tsx
@@ -6,18 +6,13 @@ export default function Portfolio() {
       className="
       w-full
       xs:w-1/2
-
       h-dvh
-
-      flex
-      flex-col
-      gap-4
       p-4
       "
     >
       <Card className="bg-input/25 h-full">
         <CardHeader>
-          <CardTitle>Portfolio</CardTitle>
+          <CardTitle className="text-xl">Portfolio</CardTitle>
         </CardHeader>
       </Card>
     </div>
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -5,13 +5,7 @@ import { heading } from "@/lib/constants";
 export default function Header() {
   return (
     <header className="flex flex-col gap-4 w-full">
-      <div className="flex gap-4 justify-between w-full">
-        <h1 className="text-6xl font-bold">
-          <p>Pablo</p>
-          <p>Apausa</p>
-        </h1>
-        <p className="text-2xl text-right text-muted">Work in progress</p>
-      </div>
+      <h1 className="text-4xl font-bold">Pablo Apausa</h1>
       <h2 className="text-2xl">{heading}</h2>
     </header>
   );
