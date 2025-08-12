4c281472824a1fa025c0dca36a5c2ce4445b00f7
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-08-12 18:33:35 +0200

    Attach Spotify authorization script

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,33 +1,41 @@
+import { useEffect, useState } from "react";
+
 import { IconBrandSpotifyFilled, IconMapPinFilled } from "@tabler/icons-react";
+import axios from "axios";
 
 export default function Footer() {
+  const [recentlyPlayed, setRecentlyPlayed] = useState<string>("");
+
+  const fetchRecentlyPlayed = async () => {
+    try {
+      const response = await axios.get("/api/spotify/recently-played");
+      setRecentlyPlayed(response.data);
+    } catch {
+      setRecentlyPlayed("Unable to load recent track");
+    }
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <footer className="flex flex-col gap-2 w-full">
       <div className="text-md text-center flex items-center gap-2">
         <IconMapPinFilled className="w-4 h-4" />
-        <p>Currently in</p>
-        <p>—</p>
-        <a
-          className="no-underline hover:underline"
-          href="https://maps.app.goo.gl/dUd1mCLUGf1VZNbu5"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Madrid, Spain
-        </a>
+        <p>
+          Based in{" "}
+          <a
+            className="no-underline hover:underline"
+            href="https://maps.app.goo.gl/dUd1mCLUGf1VZNbu5"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Madrid, Spain
+          </a>
+        </p>
       </div>
       <div className="text-md text-center flex items-center gap-2">
         <IconBrandSpotifyFilled className="w-4 h-4" />
-        <p>Last played</p>
-        <p>—</p>
-        <a
-          className="no-underline hover:underline"
-          href="https://maps.app.goo.gl/dUd1mCLUGf1VZNbu5"
-          rel="noreferrer"
-          target="_blank"
-        >
-          [To do!]
-        </a>
+        <p>Last played — {recentlyPlayed}</p>
       </div>
     </footer>
   );
---
+++ src/lib/utils/auth.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+function generateCodeVerifier(length: number): string {
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const values = crypto.getRandomValues(new Uint8Array(length));
+
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+
+async function generateCodeChallenge(codeVerifier: string): Promise<string> {
+  const data = new TextEncoder().encode(codeVerifier);
+  const digest = await crypto.subtle.digest("SHA-256", data);
+
+  return btoa(String.fromCharCode(...new Uint8Array(digest)))
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+}
+
+async function redirectToAuthCodeFlow() {
+  const verifier = generateCodeVerifier(128);
+  const challenge = await generateCodeChallenge(verifier);
+
+  localStorage.setItem("verifier", verifier);
+
+  const params = new URLSearchParams();
+  params.append("client_id", process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID!);
+  params.append("response_type", "code");
+  params.append("redirect_uri", "http://127.0.0.1:3000");
+  params.append("scope", "user-read-recently-played");
+  params.append("code_challenge_method", "S256");
+  params.append("code_challenge", challenge);
+
+  document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+export default function getAuthorizationCode() {
+  const params = new URLSearchParams(window.location.search);
+  const code = params.get("code");
+
+  if (!code) {
+    redirectToAuthCodeFlow();
+  }
+}
