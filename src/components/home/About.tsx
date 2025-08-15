fa0d326020dbc834b245d2ab44d1b06c09608024
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-08-15 20:18:09 +0200

    Implement grid design

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -1,10 +1,16 @@
+import { Separator } from "@/components/ui/separator";
 import { description } from "@/lib/constants/about";
+import { cn } from "@/lib/utils/tailwind";
 
-export default function About() {
+export default function About({ className }: { className: string }) {
   return (
-    <div className="flex flex-col gap-2">
-      <h3 className="text-lg font-bold">About me</h3>
-      <p>{description}</p>
+    <div className={cn("flex flex-col gap-4", className)}>
+      <Separator />
+      <div className="flex flex-col gap-2">
+        <h3 className="text-xl font-bold">About me</h3>
+        <p className="text-lg">{description}</p>
+      </div>
+      <Separator className="mt-auto" />
     </div>
   );
 }
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { heading } from "@/lib/constants/about";
+import { cn } from "@/lib/utils/tailwind";
 
-export default function Header() {
+export default function Header({ className }: { className: string }) {
   return (
-    <div className="flex flex-col gap-4 w-full">
+    <div className={cn("flex flex-col gap-4", className)}>
       <h1 className="text-6xl font-bold">Pablo Apausa</h1>
       <h2 className="text-3xl">{heading}</h2>
     </div>
