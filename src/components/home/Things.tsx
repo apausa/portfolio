dabc9d1325272b3dd265f1cd49dea12ef6f89664
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-08-15 20:17:38 +0200

    Rename component

--- src/components/home/Things.tsx
+++ src/components/home/Things.tsx
@@ -1,9 +1,10 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { list } from "@/lib/constants/about";
+import { cn } from "@/lib/utils/tailwind";
 
-export default function Things() {
+export default function Things({ className }: { className: string }) {
   return (
-    <Card className="flex flex-col gap-2 bg-0 p-4">
+    <Card className={cn("flex flex-col gap-2 bg-0 p-4", className)}>
       <CardHeader className="p-0 m-0">
         <CardTitle className="text-lg font-semibold p-0 m-0">
           Relevant things I&apos;ve done
