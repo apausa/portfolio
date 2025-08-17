34fd4ddd9442570994e2aee89eb698818d111397
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-08-17 15:49:31 +0200

    Update layout

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -5,9 +5,9 @@ import { cn } from "@/lib/utils/tailwind";
 
 export default function Header({ className }: { className: string }) {
   return (
-    <div className={cn("flex flex-col gap-4", className)}>
-      <h1 className="text-6xl font-bold">Pablo Apausa</h1>
-      <h2 className="text-3xl">{heading}</h2>
+    <div className={cn("flex flex-col gap-2", className)}>
+      <h1 className="text-4xl font-bold">Pablo Apausa</h1>
+      <h2 className="text-2xl">{heading}</h2>
     </div>
   );
 }
