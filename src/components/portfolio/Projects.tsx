f75b6c08548aa448e2b0c2d6dbc1a4e5ed871f1f
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-08-18 17:23:25 +0200

    Make portfolio components responsive

--- src/components/portfolio/Projects.tsx
+++ src/components/portfolio/Projects.tsx
@@ -3,12 +3,12 @@ export default function Projects() {
     <article className="flex flex-col gap-4">
       <h3 className="font-bold">Projects</h3>
       <ul className="flex flex-col gap-4">
-        <li className="flex flex-row justify-between">
+        <li className="flex flex-column xs:flex-row xs:justify-between">
           <div>
             <h4>Portfolio website</h4>
             <p>This page</p>
           </div>
-          <div className="flex flex-col items-end">
+          <div className="flex flex-col xs:items-end">
             <p>Next.js, TypeScript</p>
             <p>2025</p>
           </div>
