99b702285ebc1a214e188fc9f36326e77820db18
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-08-18 02:22:04 +0200

    Implement portfolio

--- src/app/@portfolio/page.tsx
+++ src/app/@portfolio/page.tsx
@@ -2,9 +2,8 @@ export default function Portfolio() {
   return (
     <aside
       className="
-      overflow-scroll
       bg-input/25
-      w-full
+      flex-1
       "
     >
       <div className="flex flex-col m-4 xs:m-12">
