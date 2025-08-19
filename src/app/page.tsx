c2c73441a0ee9341ae04f00a514d29d63f0b6be5
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-08-19 15:53:12 +0200

    Fix picture resizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,17 @@ import Place from "@/components/home/tags/Place";
 import Spotify from "@/components/home/tags/Spotify";
 import Time from "@/components/home/tags/Time";
 import Title from "@/components/home/Title";
+import Education from "@/components/portfolio/Education";
+import Experience from "@/components/portfolio/Experience";
 import Highlights from "@/components/portfolio/Highlights";
+import Projects from "@/components/portfolio/Projects";
 import { Separator } from "@/components/ui/separator";
 import { ModeToggle } from "@/components/ui/toggle";
 
 export default function Home() {
   return (
     <div className="relative flex flex-col xs:flex-row xs:h-dvh overflow-hidden">
-      <div className="absolute right-4 top-12 xs:top-16 xs:right-16">
+      <div className="absolute right-8 top-8">
         <ModeToggle />
       </div>
       <main
@@ -25,7 +28,7 @@ export default function Home() {
         flex
         flex-col
         xs:justify-between
-        p-4
+        px-4
         pt-12
         pb-8
         xs:p-16
@@ -53,20 +56,31 @@ export default function Home() {
         xs:flex-1
         flex
         flex-col
-        p-4
+        px-4
         pt-8
         pb-16
         xs:p-16
         xs:pl-8
         gap-8
         overflow-y-auto
-        bg-custom-selection/30
+        bg-input/30
         xs:border-l
       "
       >
-        <div className="flex-1" />
-        <div className="grid grid-cols-6 gap-4">
-          <Carrousel className="col-span-6 xs:col-span-2 xs:col-start-5" />
+        <div className="grid grid-cols-6 gap-8">
+          {/* <Experience className="col-span-6" />
+          <Projects className="col-span-6" /> */}
+          <Carrousel
+            className="
+              col-span-6
+              xs:col-span-2
+              xs:col-start-5
+              xs:h-64
+              xs:w-64
+              xs:absolute
+              xs:bottom-16
+              xs:right-16"
+          />
         </div>
       </aside>
     </div>
