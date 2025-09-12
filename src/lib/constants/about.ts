32e5f6af84bc7030382a73d472f605c5c1fad5b5
Pablo Apausa <74920898+apausa@users.noreply.github.com>
2025-09-12 11:08:50 +0200

    Update bio

--- src/lib/constants/about.ts
+++ src/lib/constants/about.ts
@@ -1,12 +1,10 @@
 import {
-  IconBookmarkFilled,
   IconBrandGithubFilled,
   IconBrandLinkedinFilled,
-  IconBrandTwitterFilled,
 } from "@tabler/icons-react";
 
 export const description = [
-  "I discovered coding through generative art during high school, then built web and mobile apps while studying design at university. This led me to pursue computer science to deepen my understanding of theoretical foundations. I now specialize in knowledge representation and reasoning, machine learning, and its applications. With this interdisciplinary background spanning human-computer interaction to software engineering, I approach challenges with a holistic perspective.",
+  "I discovered coding through generative art during high school, then built web and mobile apps while studying design at university. This led me to pursue computer science to deepen my understanding of theoretical foundations. I now specialize in deep learning, neural networks, and its applications. With this interdisciplinary background spanning human-computer interaction to software engineering, I approach challenges with a holistic perspective.",
 ];
 
 export const links = [
